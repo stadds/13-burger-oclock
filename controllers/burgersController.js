75612fa4c09eeda9e444dc1ec80d0b0d1fb991fa
STadds <staddi@gmail.com>
2020-05-03 14:56:01 -0400

    insertOne, update html name=

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,18 +5,32 @@ const router = express.Router();
 // Import the model (burger.js) to use its database functions.
 const burger = require("../models/burger.js");
 
-router.get("/",async function(req,res){
+router.get("/", async function (req, res) {
     try {
         const hbsObject = {};
         hbsObject.burgers = await burger.selectAll();
-    
+
         console.log(hbsObject);
-        res.render("index",hbsObject);
-        
+        res.render("index", hbsObject);
+
     } catch (error) {
         throw error;
     }
-   
+
+});
+
+router.post("api/burgers", async function (req, res) {
+
+    try {
+
+        let result = await burger.insertOne({ burger_name: req.body.name });
+
+        res.json({ id: result.insertId });
+
+    } catch (error) {
+        throw error;
+    }
+
 });
 
 module.exports = router;
