db1fe2e88535adbefe2588d9802961171d6035c0
STadds <staddi@gmail.com>
2020-05-03 16:30:39 -0400

    added devour-btn on click call api

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -12,12 +12,27 @@ $(function () {
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
-        }).then( function (){
+        }).then( function() {
 
             console.log("created new burger");
 
             location.reload();
         });
+
+    });
+
+    $(".devour-btn").on("click", function(event){
+
+        let id = $(this).data("id");
+
+        console.log(`burger ${id} pressed.`);
+
+        $.ajax("/api/burgers/" +id,{
+            type: "PUT"
+        }).then( function() {
+            console.log(`burger ${id} was devoured!!`);
+            location.reload();
+        })
 
     });
 
